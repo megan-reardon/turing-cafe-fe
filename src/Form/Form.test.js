f18c97f4a5fb85c65580d1e74232e9e6d7c6e142
Megan Venetianer <megan.venetianer@gmail.com>
2020-04-02 10:34:01 -0600

    finished test for Reservation cards

---
+++ src/Form/Form.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Form from './Form';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Form', () => {
+  it('should render the correct form inputs', () => {
+    
+  })
+})
--- src/Reservations/Reservations.test.js
+++ src/Reservations/Reservations.test.js
@@ -10,12 +10,18 @@ describe('Reservations', () => {
         id={3}
         name={"Meg"}
         date={"07/05"}
-        time={"7:30 pm"}
+        time={"7:30"}
         number={3}
       />);
 
       const nameEl = getByText("Meg");
+      const dateEl = getByText("07/05");
+      const timeEl = getByText("7:30 pm")
+      const numberEl = getByText("Number of guests: 3")
 
       expect(nameEl).toBeInTheDocument();
+      expect(dateEl).toBeInTheDocument();
+      expect(timeEl).toBeInTheDocument();
+      expect(numberEl).toBeInTheDocument();
   })
 })
